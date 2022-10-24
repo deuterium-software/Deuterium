0c93b07c2bc1d078f05fec357883a7cc0c7a15db
Jase <108029829+Jased-0001@users.noreply.github.com>
2022-10-23 22:59:08 -0400

    DT very basic prototype

--- dt.c
+++ dt.c
@@ -3,5 +3,29 @@
 #include "commands.h"
 
 int main(int argc, char** argv) {
+    // check argv
+
+    if (argc < 2) {
+        printf("Usage: %s <command>", argv[0]);
+    } else {
+        //check if argv[1] was defined in commands.h
+        if (strcmp(argv[1], NEW_FILE) == 0) {
+            printf("New file command");
+        } else if (strcmp(argv[1], DELETE_FILE) == 0) {
+            printf("Delete file command");
+        } else if (strcmp(argv[1], NEW_DIRECTORY) == 0) {
+            printf("New directory command");
+        } else if (strcmp(argv[1], DELETE_DIRECTORY) == 0) {
+            printf("Delete directory command");
+        } else if (strcmp(argv[1], NEW_DIRECTORY_SWITCH) == 0) {
+            printf("New directory switch command");
+        } else if (strcmp(argv[1], SAVE_TEMPLATE) == 0) {
+            printf("Save template command");
+        } else if (strcmp(argv[1], LOAD_TEMPLATE) == 0) {
+            printf("Load template command");
+        } else {
+            printf("FATAL: %s is not a valid command", argv[1]);
+        }
+    }
     
 }
