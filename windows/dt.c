6b8674936946fd1ecb758097d42a80ff42c4ba0e
Jase <108029829+Jased-0001@users.noreply.github.com>
2022-10-24 22:53:28 -0400

    oh no | added error handling
    he mad
    
    Co-Authored-By: Areg Harutyunyan <107287400+AregPrograms@users.noreply.github.com>

--- windows/dt.c
+++ windows/dt.c
@@ -3,6 +3,15 @@
 #include "../commands.h"
 #include <windows.h>
 
+/*
+* DEUTERIUM WINDOWS 2022
+*
+* AUTHORS:
+* AREG HARUTYUNYAN
+* JASE WILLIAMS
+* Github Copilot <3
+*/
+
 void error(char* text) {
     printf("\033[0;31merror: ");
     printf("\033[0m%s", text);
@@ -16,38 +25,75 @@ int main(int argc, char** argv) {
     } else {
         //check if argv[1] was defined in commands.h
         if (strcmp(argv[1], NEW_FILE) == 0) {
-            //create new file using argv[2]
+            //create new file using argv[2] for name and directory
 
             if(argv[2] == NULL) {
                 printf("Usage: %s %s <filename>", argv[0], argv[1]);
                 return 1;
             } else {
-                CreateFile(argv[2], GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
+                int* NewFile = CreateFile(argv[2], GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
+
+                if (NewFile != INVALID_HANDLE_VALUE) {
+                    printf("File successfully created.");
+                } else {
+                    error("File could not be created.");
+                }
             }
         } else if (strcmp(argv[1], DELETE_FILE) == 0) {
-            //delete file using argv[2]
+            //delete a file with argv[2] as name
 
             if(argv[2] == NULL) {
                 printf("Usage: %s %s <filename>", argv[0], argv[1]);
                 return 1;
             } else {
-                DeleteFile(argv[2]);
+                int DelFile = DeleteFile(argv[2]);
+
+                if (DelFile == 1) {
+                    printf("File successfully removed");
+                } else {
+                    error("Could not delete file.");
+                }
             }
         } else if (strcmp(argv[1], NEW_DIRECTORY) == 0) {
-            printf("not implemented");
+            // create new directory using argv[2] as path
+
+            if(argv[2] == NULL) {
+                printf("Usage: %s %s <directoryname>", argv[0], argv[1]);
+                return 1;
+            } else {
+                int NewDir = CreateDirectory(argv[2], NULL);
+
+                if (NewDir == 1) {
+                    printf("Directory successfully created.");
+                } else {
+                    error("Could not create directory.");
+                }
+            }
         } else if (strcmp(argv[1], DELETE_DIRECTORY) == 0) {
-            printf("not implemented");
+            //delete a directory with argv[2] as path to directory
+
+            if(argv[2] == NULL) {
+                printf("Usage: %s %s <directoryname>", argv[0], argv[1]);
+                return 1;
+            } else {
+                int RmDir = RemoveDirectory(argv[2]);
+
+                if (RmDir == 1) {
+                    printf("Directory successfully removed.");
+                } else {
+                    error("Could not remove directory.");
+                }
+            }
         } else if (strcmp(argv[1], NEW_DIRECTORY_SWITCH) == 0) {
-            printf("not implemented");
+            error("not implemented");
         } else if (strcmp(argv[1], SAVE_TEMPLATE) == 0) {
-            printf("not implemented");
+            error("not implemented");
         } else if (strcmp(argv[1], LOAD_TEMPLATE) == 0) {
-            printf("not implemented");
-        
+            error("not implemented");
         } else if (strcmp(argv[1], HELP) == 0) {
-            printf("not implemented");
+            error("not implemented");
         } else {
-            printf("FATAL: %s is not a valid command", argv[1]);
+            error("command not found.");
             return 1;
         }
     }
