0ffb3109e203161c371081fcc301ad79eaa8add8
Jase <108029829+Jased-0001@users.noreply.github.com>
2022-10-25 21:41:32 -0400

    beta templates
    beta 10m.25d.22y

--- windows/dt.c
+++ windows/dt.c
@@ -87,9 +87,91 @@ int main(int argc, char** argv) {
         } else if (strcmp(argv[1], NEW_DIRECTORY_SWITCH) == 0) {
             error("not implemented");
         } else if (strcmp(argv[1], SAVE_TEMPLATE) == 0) {
-            error("not implemented");
+            //save a template with argv[2] as name
+
+            //first, copy the folder and everything inside specified and paste it in the templates folder
+            //templates folder = C:\Program Files\DeuTerium\templates
+
+            if(argv[2] == NULL) {
+                printf("Usage: %s %s <templatename>", argv[0], argv[1]);
+                return 1;
+            } else {
+                int CopyDir = CopyFile(argv[2], "C:\\Program Files\\DeuTerium\\templates\\", 0);
+
+                if (CopyDir == 1) {
+                    printf("Template successfully saved.");
+                } else {
+                    error("Could not save template.");
+                }
+            }
+
+            //now we register the template in the "pointer" file
+            //pointer file = C:\Program Files\DeuTerium\templates\pointer.txt
+
+            FILE* PointerFile = fopen("C:\\Program Files\\DeuTerium\\templates\\pointer.txt", "a");
+
+            if (PointerFile == NULL) {
+                error("Could not open pointer file. Template may not work.");
+            } else {
+                fprintf(PointerFile, "%s ", argv[2]);
+            }
+
         } else if (strcmp(argv[1], LOAD_TEMPLATE) == 0) {
-            error("not implemented");
+            //load a template with argv[2] as name
+            //this basically just copies the template to the current directory
+
+            //first, we need to check if the template exists
+            //we do this by checking the pointer file
+
+            FILE* PointerFile = fopen("C:\\Program Files\\DeuTerium\\templates\\pointer.txt", "r");
+
+            if (PointerFile == NULL) {
+                error("Could not open pointer file.");
+            } else {
+                char* PointerFileContents = malloc(1000); //memory leaks! :D
+                fgets(PointerFileContents, 1000, PointerFile);
+
+                if (strstr(PointerFileContents, argv[2]) == NULL) {
+                    error("Template does not exist.");
+                } else {
+                    //now we copy the template to the current directory
+                    int CopyDir = CopyFile("C:\\Program Files\\DeuTerium\\templates\\", argv[2], 0);
+
+                    if (CopyDir == 1) {
+                        printf("Template successfully loaded.");
+                    } else {
+                        error("Could not load template.");
+                    }
+                }
+            }
+        } else if (strcmp(argv[1], DELETE_TEMPLATE) == 0) {
+            //delete a template with argv[2] as name
+
+            //first, we need to check if the template exists
+            //we do this by checking the pointer file.. again
+
+            FILE* PointerFile = fopen("C:\\Program Files\\DeuTerium\\templates\\pointer.txt", "r");
+
+            if (PointerFile == NULL) {
+                error("Could not open pointer file.");
+            } else {
+                char* PointerFileContents = malloc(1000); //memory leaks! :D
+                fgets(PointerFileContents, 1000, PointerFile);
+
+                if (strstr(PointerFileContents, argv[2]) == NULL) {
+                    error("Template does not exist.");
+                } else {
+                    //now we delete the template
+                    int DelDir = RemoveDirectory("C:\\Program Files\\DeuTerium\\templates\\");
+
+                    if (DelDir == 1) {
+                        printf("Template successfully removed.");
+                    } else {
+                        error("Could not remove template.");
+                        printf("this is a beta. understandable have a nice day");
+                    }
+                }
+            }
         } else if (strcmp(argv[1], HELP) == 0) {
             error("not implemented");
         } else {
